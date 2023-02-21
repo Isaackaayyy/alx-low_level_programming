948ed867d0a61b123b36592b71cfdbc8295aa653
IsaacKaayyy <kammaokoh@gmail.com>
2023-02-21 03:57:16 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a program that prints _putchar, followed by a new line
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char Mystr[] = "_putchar";
+
+	puts(Mystr);
+
+	return (0);
+}
