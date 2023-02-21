0c01eb562415d9adb7ab8f5995867686b53437ef
IsaacKaayyy <kammaokoh@gmail.com>
2023-02-21 08:55:36 -0800

    print combination3

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Write a function that prints the alphabet
+ *
+ * Rrturn: Always 0
+ */
+
+int main(void)
+{
+	char Mystr[] = 1-main.c;
+
+	puts(Mystr);
+
+	return (0);
+}
