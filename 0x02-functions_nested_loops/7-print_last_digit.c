8df4ed15274c645bf10b8d96170953393b502da7
IsaacKaayyy <kammaokoh@gmail.com>
2023-02-22 15:50:59 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - A function that prints the last digit of a number
+ *
+ * @n: first value
+ *
+ * Return: integer
+ */
+
+int print_last_digit(int n)
+{
+
