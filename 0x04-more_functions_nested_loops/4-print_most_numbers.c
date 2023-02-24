1a88da7983984836e96557856e07589fd77e57b5
IsaacKaayyy <kammaokoh@gmail.com>
2023-02-24 03:39:50 -0800

    #include main.h

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,14 +10,14 @@ void print_most_numbers(void)
 {
 	int a;
 
-	while (a = 0; a <= 9; a++)
-	
-		if (a == 2 && a == 4)
+	for (a = 0; a <= 9; a++)
+	{
+		if (a != 2 && a != 4)
 		{
 			continue;
 		}
 		else
-		{	
+		{
 			_putchar(a + '0');
 		}
 
