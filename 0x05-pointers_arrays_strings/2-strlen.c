55d7db5b9d8562d882ac801d236b4b59071622f5
IsaacKaayyy <kammaokoh@gmail.com>
2023-02-28 06:58:49 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - length of a string
+ *
+ * @s: first value
+ *
+ * Return: unsigned int
+ */
+
+int _strlen(char *s)
+{
+
+	return (strlen(s));
+}
