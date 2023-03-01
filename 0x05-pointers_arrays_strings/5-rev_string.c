8926d6c2078ab236103f35597422cd640261b948
IsaacKaayyy <kammaokoh@gmail.com>
2023-02-28 16:24:32 -0800

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,5 +11,20 @@
 
 void rev_string(char *s)
 {
-	strrev(s);
+	int i, strlen, temp, halfstr;
+
+	i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	strlen = i - 1;
+	halfstr = strlen / 2;
+
+	for (i = 0; i <= halfstr; i++)
+	{
+		temp = s[i];
+		s[i] = s[strlen];
+		s[strlen] = temp;
+		strlen--;
+	}
 }
