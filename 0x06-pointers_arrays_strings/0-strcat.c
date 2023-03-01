2597dc94e529b3bf48737a6192dcc6c5a04d63f1
IsaacKaayyy <kammaokoh@gmail.com>
2023-03-01 06:58:55 -0800

    Appending a string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcat - Apendind a string to another string
+ *
+ * @dest: The first string
+ * @src: String to be appended to the next string
+ *
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
