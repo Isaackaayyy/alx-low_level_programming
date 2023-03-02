4b90f28022af16a371aa0d927b0dae9c8e72fde7
IsaacKaayyy <kammaokoh@gmail.com>
2023-03-02 15:12:48 -0800

    Reversing an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reversing a string
+ *
+ * @a: array of integers
+ * @n: integer
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int i;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
