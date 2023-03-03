2cb02a56537685beb9f9b55611287a4779761b24
IsaacKaayyy <kammaokoh@gmail.com>
2023-03-03 11:06:54 -0800

    Changing the characters of a string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changing lowercase characters to uppercase characters.
+ *
+ * @s: Array
+ *
+ * Return: char
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
