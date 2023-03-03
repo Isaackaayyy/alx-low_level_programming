c008ad10f2ca9ee7f3ba638a00bea3076d040c3a
IsaacKaayyy <kammaokoh@gmail.com>
2023-03-03 12:42:28 -0800

    capitalizing words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,8 +14,25 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 97 && s[i] <= 122)
-			s[i] = s[i] - 32;
+		if (i == 0)
+		{
+			if (s[i] >= 97 && s[i] <= 122)
+				s[i] = s[i] - 32;
+			continue;
+		}
+		else if (s[i] == 32)
+		{
+			i++;
+			if (s[i] >= 97 && s[i] <= 122)
+				s[i] = s[i] - 32;
+			continue;
+		}
+		else
+		{
+			if (s[i] >= 65 && s[i] <= 90)
+				s[i] = s[i] + 32;
+			continue;
+		}
 	}
 	return (s);
 }
