9a378089420522a9d198e4c7f470d09101170daf
IsaacKaayyy <kammaokoh@gmail.com>
2023-03-03 15:52:44 -0800

    capitalizing words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,28 +14,28 @@ char *cap_string(char *s)
 	int trigger;
 	char nots[] = ",;.!?(){}\n\t\" ";
 
-	for (i = 0, trigger = 0; str[i] != '\0'; i++)
+	for (i = 0, trigger = 0; s[i] != '\0'; i++)
 	{
-		if (str[0] > 96 && str[0] < 123)
+		if (s[i] >= 97 && s[i] <= 122)
 			trigger = 1;
 		for (c = 0; nots[c] != '\0'; c++)
 		{
-			if (nots[c] == str[i])
+			if (nots[c] == s[i])
 				trigger = 1;
 		}
 
 		if (trigger)
 		{
-			if (str[i] > 96 && str[i] < 123)
+			if (s[i] >= 97 && s[i] <= 122)
 			{
-				str[i] -= 32;
-				rigger = 0;
-			}
-			else if (str[i] > 64 && str[i] < 91)
+				s[i] = s[i] -  32;
 				trigger = 0;
-			else if (str[i] > 47 && str[i] < 58)
+			}
+			else if (s[i] >= 65 && s[i] <= 90)
+				trigger = 0;
+			else if (s[i] >= 48 && s[i] <= 57)
 				trigger = 0;
 		}
 	}
-	return (str);
+	return (s);
 }
