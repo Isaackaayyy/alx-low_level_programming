e23c28fdec7bd53a4ac49ff2ddb6377e71827593
IsaacKaayyy <kammaokoh@gmail.com>
2023-03-06 11:06:38 -0800

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - A function that locates a character in a string.
+ *
+ * @s: Pionter string
+ * @c: Character to locate
+ *
+ * Return: char
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s);
+		else
+		i++;
+	}
+}
