056eefaf9e138393e3b5fcf9b594c4453e837ee6
IsaacKaayyy <kammaokoh@gmail.com>
2023-03-08 15:37:32 -0800

    length of a string with recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,11 +10,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int c;
+	int i = 0;
 
-	if (s == '\0')
-		return (0);
-
-	c = 1 + _strlen_recursion(s + 1);
-	return (c);
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	}
+	return (i);
 }
