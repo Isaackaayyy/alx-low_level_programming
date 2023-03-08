8243a1bd9bbd21ae2640886a0e3d996d1783d10d
IsaacKaayyy <kammaokoh@gmail.com>
2023-03-08 15:23:11 -0800

    checking for prime numbers with recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checking for prime numbers
+ *
+ * @n: input value
+ *
+ * Return: integer
+ */
+
+int is_prime_number(int n)
+{
+	int i;
+	i = n - 1;
+
+	if (n == 1 || n < 0)
+		return (0);
+	if (n % i == 0)
+		return (1);
+	n = n;
+	return (n);
+}
