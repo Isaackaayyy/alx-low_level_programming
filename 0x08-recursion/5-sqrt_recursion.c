c89e03c9910607e89676cdef37326f8f61caa974
IsaacKaayyy <kammaokoh@gmail.com>
2023-03-09 13:03:58 -0800

    Square root with recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,11 +8,32 @@
  * Return: integer
  */
 
+int _sqrt(int n, int i);
+
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	if (n == 0)
-		return (0);
-	return (n);
+	int val, i = 0;
+
+	val = _sqrt(n, i);
+
+	return (val);
 }
+
+/**
+ * _sqrt -  function to help with the square root
+ *
+ * @n: value acted on
+ * @i: counter value
+ *
+ * Return: integer
+ */
+
+int _sqrt(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i < n)
+		return (_sqrt(n, ++i));
+	return (-1);
+}
+
