bcfa2af2c85f897a160908e836005373dd249dcf
IsaacKaayyy <kammaokoh@gmail.com>
2023-03-09 13:33:53 -0800

    Prime numbers with recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,20 +4,38 @@
  * is_prime_number - checking for prime numbers
  *
  * @n: input value
+ * @i: counter
  *
  * Return: integer
  */
 
+int _prime(int n, int i);
+
 int is_prime_number(int n)
 {
-	int i;
+	int val, i = 2;
 
-	i = n - 1;
+	val = _prime(n, i);
 
-	if (n == 1 || n < 0)
+	return (val);
+}
+
+/**
+ * _prime - function assisting in prime number
+ *
+ * @n: input value
+ * @i: counter
+ *
+ * Return: integer
+ */
+
+int _prime(int n, int i)
+{
+	if (n == 1 || n <  0)
 		return (0);
-	if (n % i == 0)
-		return (1);
-	n = n;
-	return (n);
+	if (n % i == 0 && i != n)
+		return (0);
+	else if (i < n)
+		return (_prime(n, ++i));
+	return (1);
 }
