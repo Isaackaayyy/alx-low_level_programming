e6accdce5870b8d62317444bcea9ef9a53fbe7c4
IsaacKaayyy <kammaokoh@gmail.com>
2023-03-10 12:39:40 -0800

    printing the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing the mane of the program
+ *
+ * @argc: index
+ * @argv: pointer to array of string
+ *
+ * Return: integer
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
