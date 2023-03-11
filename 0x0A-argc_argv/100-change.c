1f4fdd8ca5e47400dd2d0fec1dd15e4990363d56
IsaacKaayyy <kammaokoh@gmail.com>
2023-03-11 03:23:41 -0800

    Calculating change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -34,27 +34,18 @@ int main(int argc, char *argv[])
 		if ((cents - 25) >= 0)
 		{
 			cents -= 25;
-			continue;
 		}
 		if ((cents - 10) >= 0)
 		{
 			cents -= 10;
-			continue;
 		}
 		if ((cents - 5) >= 0)
 		{
 			cents  -= 5;
-			continue;
 		}
 		if ((cents - 2) >= 0)
 		{
 			cents -= 2;
-			continue;
-		}
-		if ((cents - 1) >= 0)
-		{
-			cents -= 1;
-			continue;
 		}
 		cents--;
 	}
