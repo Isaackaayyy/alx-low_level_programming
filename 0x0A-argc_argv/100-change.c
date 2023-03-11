6c269daf30d65a8d58c50eb1fa5891535688b7f2
IsaacKaayyy <kammaokoh@gmail.com>
2023-03-11 03:36:00 -0800

    Calculating change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,10 +4,8 @@
 /**
  * main - Write a program that prints the minimum number of coins
  * to make change for an amount of money
- *
  * @argc: counter
  * @argv: vector
- *
  * Rerurn: integer
  */
 
@@ -32,13 +30,25 @@ int main(int argc, char *argv[])
 	{
 		coins++;
 		if ((cents - 25) >= 0)
+		{
 			cents -= 25;
+			continue;
+		}
 		if ((cents - 10) >= 0)
+		{
 			cents -= 10;
+			continue;
+		}
 		if ((cents - 5) >= 0)
+		{
 			cents  -= 5;
+			continue;
+		}
 		if ((cents - 2) >= 0)
+		{
 			cents -= 2;
+			continue;
+		}
 		if ((cents - 1) >= 0)
 			cents -= 1;
 		cents--;
