dc0999e95d581612ad55d6247bb24ac22cba5b82
IsaacKaayyy <kammaokoh@gmail.com>
2023-03-14 16:32:50 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,8 +15,10 @@ char *str_concat(char *s1, char *s2)
 	char *a;
 	int i, j, k, l, m, n;
 
-	if (s1 || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 		continue;
