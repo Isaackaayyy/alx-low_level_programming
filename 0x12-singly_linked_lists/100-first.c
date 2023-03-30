8a8bb176a7c8bd6e6bd07d49c9d4897a443d78ab
Isaackaayyy <kammaokoh@gmail.com>
2023-03-30 16:54:45 -0700

    Print before the main programme

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void _print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\\nI bore my house upon my back!\\n\n");
+}
+
+int print(void)
+{
+	_print_message();
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+
+	return (0);
+}
