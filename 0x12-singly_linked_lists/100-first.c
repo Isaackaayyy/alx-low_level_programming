1ad7ae5d681358109a8ed5d8e3ed5a30d7f9006b
Isaackaayyy <kammaokoh@gmail.com>
2023-03-30 17:15:16 -0700

    Print before the main programme

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
+void _print_message(void) __attribute__((constructor));
+
+/**
+ * _print_message - Function before the main function
+ */
+
+
 void _print_message(void)
 {
-	printf("You're beat! and yet, you must allow,\\nI bore my house upon my back!\\n\n");
+	printf("You're beat! and yet, you must allow,\\nI bore my house upon ");
+	printf("my back!\\n\n");
 }
 
-int print(void)
+/**
+ * _print - Main function
+ * Return: 0
+ */
+
+int _print(void)
 {
-	_print_message();
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	printf("(A tortoise, having pretty good sense of a hare's nature, ");
+	printf("challenges one to a race.)\n");
 
 	return (0);
 }
