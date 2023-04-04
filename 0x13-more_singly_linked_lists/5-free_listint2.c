1b8759d400ec1f8c421d7c591f8eadc81a4ec138
Isaackaayyy <kammaokoh@gmail.com>
2023-04-04 14:03:22 -0700

    sets head to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *m;
 
+	if (head == NULL)
+		return;
+
 	while (head != NULL)
 	{
 		m = *head;
-		*head = (*head)->next;
 		free(m);
 	}
 	*head = NULL;
