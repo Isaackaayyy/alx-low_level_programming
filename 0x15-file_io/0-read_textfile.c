6fb95974419b81d52890a0c7307e65122b405922
Isaackaayyy <kammaokoh@gmail.com>
2023-04-10 08:14:02 -0700

    reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile - read a text file and prints it to the POSIX standard output
+ * @filename: Pointer to the file to read
+ * @letters: number of letters that could be read and printed from the file
+ * Return: number of letters read and priinted from the file
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fptr;
+	char a;
+	size_t r = 0;
+
+	if (filename == NULL)
+		return (r);
+
+	fptr = fopen(filename, "r");
+	if (fptr == NULL)
+		return (r);
+
+	letters = 0;
+
+	while (!feof(fptr))
+	{
+		a = fgetc(fptr);
+		printf("%c", a);
+		letters++;
+	}
+
+	fclose(fptr);
+	return (letters);
+}
