b2918ed24010569856a9db9cc283e860a92288b2
Isaackaayyy <kammaokoh@gmail.com>
2023-05-05 16:05:30 -0700

    Prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,12 +7,14 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
+	if ((n >> 1) == 0 && n == 0)
 	{
 		_putchar('0');
 		return;
 	}
 
 	print_binary(n >> 1);
+
 	_putchar((n & 1) ? '1' : '0');
+
 }
