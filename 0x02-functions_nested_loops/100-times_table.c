d274e1f7d3dabfefc6b916fb0474c93f475baefe
Isaackaayyy <kammaokoh@gmail.com>
2023-05-10 09:00:15 -0700

    function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,31 +9,40 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			k = i * j;
-			if (j > 0)
-				_putchar(' ');
-
-			if (k < 10 && j > 0)
-				_putchar(' ');
-
-			if (k < 100)
-				_putchar(' ');
-
-			if (k >= 100)
-				_putchar((k / 100) % 10 + '0');
-
-			if (k >= 10)
-				_putchar((k / 10) % 10 + '0');
-
-			_putchar(k % 10 + '0');
-
-			if (j < n)
-				_putchar(',');
+			for (j = 0; j <= n; j++)
+			{
+				k = i * j;
+				if (k >= 100)
+				{
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				if (k >= 10)
+				{
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				if (k <= 9 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				if (k == 0 && j == 0)
+					_putchar('0');
+				if (j != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
