6bbeb975a1ae2672f2189f289afd50729ee6a219
Isaackaayyy <kammaokoh@Kays-Computer.localdomain>
2023-08-04 03:07:38 +0100

    Bit Manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n:  decimal number
+ */
+
+void print_binary(unsigned long int n)
+{
+	if ((n >> 1) == 0 && n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n >> 1 != 0)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) ? '1' : '0');
+
+}
